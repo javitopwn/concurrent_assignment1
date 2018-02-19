80b75f3a8289fe829c57bdda3e2a94de60f913fc
usuario <usuario@161.67.196.151>
2018-02-19 13:25:43 +0100

    Ej1

--- A/Reference.java
+++ A/Reference.java
@@ -1,8 +1,18 @@
 package concurrent_assignment1.A;
 
 /**
- * Get the reference of the main thread haber si  e mmuero */
+ * Get the reference of the main thread */
 
 public class Reference {
+       Thread h = Thread.currentThread();
 
+public static void main(String[] args) {
+        int i;
+        for(i=0;i<100;i++){
+            if((i%2)!=0)
+            System.out.println(i);
+        }
+}
+    
+    
 }
