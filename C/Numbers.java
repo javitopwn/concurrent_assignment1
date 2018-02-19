6eb0adcabd0aa6bc774ace25a303dc2f477b025f
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-02-19 14:18:04 +0100

    Update Numbers.java

--- C/Numbers.java
+++ C/Numbers.java
@@ -9,5 +9,17 @@ package concurrent_assignment1.C;
  */
 
 public class Numbers {
-
+    
+    public static void main(String[] args) {
+    R_PrintID o1=new R_PrintID(1);
+    R_PrintID o2=new R_PrintID(2);
+    R_PrintID o3=new R_PrintID(3);
+    R_PrintID o4=new R_PrintID(4);
+    R_PrintID o5=new R_PrintID(5);
+    o1.run();
+    o2.run();
+    o3.run();
+    o4.run();
+    o5.run();
+    }
 }
