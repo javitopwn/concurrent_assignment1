46bf5da23f120821617dbc10dd3e924aea28fe3b
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-02-19 20:43:56 +0100

    Update R_PrintID.java

--- C/R_PrintID.java
+++ C/R_PrintID.java
@@ -7,6 +7,8 @@ package concurrent_assignment1.C;
  * @author yournamehere
  *
  */
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class R_PrintID implements Runnable{
     int ID;
@@ -27,21 +29,4 @@ public class R_PrintID implements Runnable{
 }
 
 
-public class T_PrintID extends Thread implements Runnable {
-    int ID;
-    public T_PrintID(int x){
-        this.ID=x;
-    }
-    @Override
-    public void run(){
-        for (int i = 0; i < 10; i++) {
-            System.out.println(this.ID);
-            try {
-                Thread.sleep((int)(Math.random()*1000));
-            } catch (InterruptedException ex) {
-                System.out.println(ex.toString());
-            }
-       
-    }
-}
-}
+
