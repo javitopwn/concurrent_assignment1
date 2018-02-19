cf9af261adba21c528f94029d53e4f8b6afe62b0
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-02-19 13:45:56 +0100

    Update Reference.java

--- A/Reference.java
+++ A/Reference.java
@@ -4,13 +4,18 @@ package concurrent_assignment1.A;
  * Get the reference of the main thread */
 
 public class Reference {
-       Thread h = Thread.currentThread();
-
-public static void main(String[] args) {
-        int i;
+    
+public static void main(String[] args) throws InterruptedException {
+        Thread h = Thread.currentThread();
+        int i;      
+        System.out.println(h.getName());
+        h.setName("Thread");
         for(i=0;i<100;i++){
-            if((i%2)!=0)
-            System.out.println(i);
+            if((i%2)!=0){
+            System.out.println(i+" Thread name: "+h.getName());
+            h.sleep(2000);
+            }
+            //else h.sleep(2000);
         }
 }
     
