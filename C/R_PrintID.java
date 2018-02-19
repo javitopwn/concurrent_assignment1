becc01274c27612a6df6dc9075ea07667fe4021b
javitopwn <34965674+javitopwn@users.noreply.github.com>
2018-02-19 19:59:22 +0100

    Update R_PrintID.java

--- C/R_PrintID.java
+++ C/R_PrintID.java
@@ -25,3 +25,23 @@ public class R_PrintID implements Runnable{
         }
     }
 }
+
+
+public class T_PrintID extends Thread implements Runnable {
+    int ID;
+    public T_PrintID(int x){
+        this.ID=x;
+    }
+    @Override
+    public void run(){
+        for (int i = 0; i < 10; i++) {
+            System.out.println(this.ID);
+            try {
+                Thread.sleep((int)(Math.random()*1000));
+            } catch (InterruptedException ex) {
+                System.out.println(ex.toString());
+            }
+       
+    }
+}
+}
