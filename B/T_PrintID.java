2c44cb2514caa37f32ae29ea1cbc66603857d0b7
javitopwn <34965674+javitopwn@users.noreply.github.com>
2018-02-19 19:57:26 +0100

    Update T_PrintID.java

--- B/T_PrintID.java
+++ B/T_PrintID.java
@@ -1,4 +1,4 @@
-public class T_PrintID extends Thread implements Runnable {
+public class T_PrintID extends Thread {
     int ID;
     public T_PrintID(int x){
         this.ID=x;
@@ -7,10 +7,6 @@ public class T_PrintID extends Thread implements Runnable {
     public void run(){
         for (int i = 0; i < 10; i++) {
             System.out.println(this.ID);
-            try {
-                Thread.sleep((int)(Math.random()*1000));
-            } catch (InterruptedException ex) {
-                System.out.println(ex.toString());
             }
        
     }
