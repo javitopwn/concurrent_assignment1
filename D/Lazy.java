6c117b08f9aa84c9fba5cbe2735fcde6f9af5ac6
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-02-25 18:37:12 +0100

    Update Lazy.java

--- D/Lazy.java
+++ D/Lazy.java
@@ -8,10 +8,32 @@ package concurrent_assignment1.D;
  * - If Hurry interrupts Lazy before it is finished, Lazy claims: “That’s not cricket, please play the game!”.
  * - If Lazy finishes before being interrupted, it says “I am ready, the early bird catches the worm!”.
  * 
- * @author yournamehere
+ * @author Viktitors
  *
  */
 
-public class Lazy {
-
+public class Lazy extends Thread implements Runnable{
+    
+    @Override
+    public void run(){        
+        int ran=(int)(Math.random()*8+2);
+        int ran2=(int)(Math.random()*2);
+        
+        for (int i = 0; i < ran; i++) 
+        {   
+            if(this.isInterrupted())System.out.println("That’s not cricket, please play the game!\n");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException ex) {
+                //Logger.getLogger(Lazy.class.getName()).log(Level.SEVERE, null, ex);
+            }
+                switch(ran2)
+                {
+                case 0 : System.out.println("I am dressing up...\n");
+                case 1 : System.out.println("Just a sec, please...\n");
+                case 2 : System.out.println("These clothes do not suit me...\n");
+                }                
+        }
+        if(!this.isInterrupted())System.out.println("I am ready, the early bird catches the worm!\n");
+    }
 }
