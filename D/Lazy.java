aefed640eaebda9fbcf81e57e1db526beda9c422
Viktitors <34943687+Viktitors@users.noreply.github.com>
2018-02-26 13:00:59 +0100

    if/else out of the for loop

--- D/Lazy.java
+++ D/Lazy.java
@@ -12,29 +12,33 @@ package concurrent_assignment1.D;
  *
  */
 
-public class Lazy extends Thread implements Runnable{
+public class Lazy extends Thread {
     
     @Override
     public void run(){        
         int ran=(int)(Math.random()*8+2);
-                
-        for (int i = 0; i < ran; i++) 
+        int ran2;        
+        for (int i = 0; i < ran && !Thread.currentThread().isInterrupted(); i++) 
         {   
-            int ran2=(int)(Math.random()*2);
-
-            if(this.isInterrupted())System.out.println("That’s not cricket, please play the game!\n");
+            ran2=(int)(Math.random()*3+1);
+            switch(ran2)
+                {
+                case 1 : System.out.println("I am dressing up...\n"); break;
+                case 2 : System.out.println("Just a sec, please...\n");break;
+                case 3 : System.out.println("These clothes do not suit me...\n");break;
+                } 
+            
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
-                //Logger.getLogger(Lazy.class.getName()).log(Level.SEVERE, null, ex);
+                interrupt();
             }
-                switch(ran2)
-                {
-                case 0 : System.out.println("I am dressing up...\n"); break;
-                case 1 : System.out.println("Just a sec, please...\n");break;
-                case 2 : System.out.println("These clothes do not suit me...\n");break;
-                }                
+                               
         }
-        if(!this.isInterrupted())System.out.println("I am ready, the early bird catches the worm!\n");
+        
+        if(!Thread.currentThread().isInterrupted())System.out.println("I am ready, the early bird catches the worm!\n");
+        
+        else System.out.println("That’s not cricket, please play the game!\n");
+        
     }
 }
